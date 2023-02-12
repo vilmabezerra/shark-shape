89813ebbc5fbfc9fc2f25c3f23374ac96f0b3212
vilmabezerra <vilmabezerraalves@gmail.com>
2023-02-12 16:59:24 -0300

    chore: add prisma

---
+++ server/api/prisma/migrations/20230212191418_init/migration.sql
@@ -0,0 +1,88 @@
+-- CreateTable
+CREATE TABLE "WorkoutPlan" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "WorkoutPlan_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Workout" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "workoutPlanId" TEXT NOT NULL,
+
+    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutInfo" (
+    "id" TEXT NOT NULL,
+    "workoutId" TEXT NOT NULL,
+    "exerciseId" INTEGER NOT NULL,
+    "series" INTEGER NOT NULL,
+    "repetitions" INTEGER NOT NULL,
+    "workoutItemId" TEXT,
+
+    CONSTRAINT "WorkoutInfo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Exercise" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "img" TEXT NOT NULL,
+    "workoutId" TEXT,
+
+    CONSTRAINT "Exercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WorkoutItem" (
+    "id" TEXT NOT NULL,
+    "startDate" TIMESTAMP(3) NOT NULL,
+    "endDate" TIMESTAMP(3) NOT NULL,
+    "workoutId" TEXT,
+
+    CONSTRAINT "WorkoutItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ExerciseItem" (
+    "id" TEXT NOT NULL,
+    "exerciseId" TEXT NOT NULL,
+    "payload" TEXT NOT NULL,
+    "workoutItemId" TEXT,
+
+    CONSTRAINT "ExerciseItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "WorkoutPlan_name_key" ON "WorkoutPlan"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Workout_name_key" ON "Workout"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Exercise_name_key" ON "Exercise"("name");
+
+-- AddForeignKey
+ALTER TABLE "Workout" ADD CONSTRAINT "Workout_workoutPlanId_fkey" FOREIGN KEY ("workoutPlanId") REFERENCES "WorkoutPlan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkoutInfo" ADD CONSTRAINT "WorkoutInfo_workoutId_fkey" FOREIGN KEY ("workoutId") REFERENCES "Workout"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkoutInfo" ADD CONSTRAINT "WorkoutInfo_workoutItemId_fkey" FOREIGN KEY ("workoutItemId") REFERENCES "WorkoutItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Exercise" ADD CONSTRAINT "Exercise_workoutId_fkey" FOREIGN KEY ("workoutId") REFERENCES "Workout"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WorkoutItem" ADD CONSTRAINT "WorkoutItem_workoutId_fkey" FOREIGN KEY ("workoutId") REFERENCES "Workout"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ExerciseItem" ADD CONSTRAINT "ExerciseItem_workoutItemId_fkey" FOREIGN KEY ("workoutItemId") REFERENCES "WorkoutItem"("id") ON DELETE SET NULL ON UPDATE CASCADE;
